08c524e650688361a34859e286d3001670eb05ab
jinusong14@gmail.com <jinusong14@gmail.com>
2024-09-12 22:37:01 +0900

    [송진우] - 회원가입 화면 기능 구현 - 비밀번호 암호화 기능 추가

---
+++ src/main/java/com/jinu/memo/user/service/UserService.java
@@ -0,0 +1,30 @@
+package com.jinu.memo.user.service;
+
+import org.springframework.stereotype.Service;
+
+import com.jinu.memo.common.MD5HashingEncoder;
+import com.jinu.memo.user.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	private UserRepository userRepository;
+	
+	// 생성자가 autowired를 위한 것 하나만 존재하는 경우 autowired 생략
+	// @Autowired
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public int addUser(
+			String loginId
+			, String password
+			, String name
+			, String email){
+		
+		// 암호화
+		String encryptPassword = MD5HashingEncoder.edcode(password);
+		
+		return userRepository.insertUser(loginId, encryptPassword, name, email);
+	}
+}
